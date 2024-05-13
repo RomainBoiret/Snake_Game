36a0f4512de1a12e5054b901a18af09d61cf6a00
Romain <96925387+RomainBoiret@users.noreply.github.com>
2024-05-13 17:15:50 -0400

    feature

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,129 @@
-const game = document.getElementById('game');
+const game = document.querySelector(".game");
 
-let cols = 20;
-let rows = 16;
+const cols = 30;
+const rows = 30;
 
+let gameOff = false;
+let setIntervalID;
 let snakeBody = [];
 
-// Création de la grille
-function generateGrid()
+let newFoodX = 0;
+let newFoodY = 0;
+
+let snakeX = 5;
+let snakeY = 15;
+
+let stepX = 0;
+let stepY = 0;
+
+function generateFood()
 {
-    for (let i = 0; i < rows; i++) 
-    {
-        for (let j = 0; j < cols; j++) 
-        {
-            let cell = document.createElement('div');
-    
-            cell.classList.add('cell');
-            cell.setAttribute('data-x', j);
-            cell.setAttribute('data-y', i);
-    
-            game.appendChild(cell);
-        }
-    }
+    newFoodX = Math.floor(Math.random() * cols) + 1;
+    newFoodY = Math.floor(Math.random() * rows) + 1;
 }
-  
-// Ecoute des clics sur les cellules
-game.addEventListener('click', (event) => {
-
-    if (event.target.classList.contains('cell')) 
-    {
-        let x = event.target.getAttribute('data-x');
-        let y = event.target.getAttribute('data-y');
-
-        console.log(`Coordonnées de la cellule : x = ${x}, y = ${y}`);
-    }
-});
 
 function addFood()
 {
-    let x = Math.floor(Math.random() * cols);
-    let y = Math.floor(Math.random() * rows);
+    let newFood = `<div class="food" style="grid-area: ${newFoodY} / ${newFoodX}"></div>`;
 
-    let cell = document.querySelector(`.cell[data-x="${x}"][data-y="${y}"]`);
-
-    let newFood = document.createElement('div');
-
-    newFood.classList.add('food');
-
-    cell.appendChild(newFood);
+    return newFood;
 }
 
 function initSnake()
 {
-    
+    for (let i = snakeBody.length - 1; i > 0; i--)
+    {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY];
+
+    snakeX += stepX;
+    snakeY += stepY;
+
+    if (snakeX <= 0 || snakeX > cols || snakeY <= 0 || snakeY > rows)
+    {
+        gameOff = true;
+    }
+
+    let snake = "";
+
+    for (let i = 0; i < snakeBody.length; i++)
+    {
+        snake += `<div class="snake" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        // if (i !== 0 && snakeBody)
+    }
+
+    return snake;
+}
+
+function colision()
+{
+    generateFood();
+    snakeBody.push([newFoodX, newFoodY]);
+}
+
+function initGame()
+{
+    if (gameOff)
+        return gameOver();
+
+    if (newFoodX === snakeX && newFoodY === snakeY)
+        colision();
+
+    game.innerHTML = addFood() + initSnake();
+}
+
+function gameOver()
+{
+    clearInterval(setIntervalID);
+    alert("Game Over! Press OK to replay...");
+    location.reload();
+}
+
+function controlers(e)
+{
+    switch(e.key)
+    {
+        case "ArrowUp": case "w":
+            if (stepY != 1)
+            {
+                stepX = 0;
+                stepY = -1;
+            }
+            break;
+        case "ArrowDown": case "s":
+            if (stepY != -1)
+            {
+                stepX = 0;
+                stepY = 1;
+            }
+            break;
+        case "ArrowLeft": case "a":
+            if (stepX != 1)
+            {
+                stepX = -1;
+                stepY = 0;
+            }
+            break;
+        case "ArrowRight": case "d":
+            if (stepX != -1)
+            {
+                stepX = 1;
+                stepY = 0;
+            }
+            break;
+    }
 }
 
 function startGame()
 {
-    // Générer la grille
-    generateGrid();
-
-    // Créer le snake
-    initSnake()
-
     // Ajouter nourriture
-    addFood();
+    generateFood();
+
+    // Initialiser la partie
+    setIntervalID = setInterval(initGame, 130);
+
+    // Gerer les touches
+    document.addEventListener("keydown", controlers);
 }
